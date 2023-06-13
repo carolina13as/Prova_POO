e73838f2b7dae1f5f9d2f93cf84f27d858ac357a
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2023-06-13 15:36:59 -0400

    Terceiro commit

--- Prova de POO 2º bimestre/Gabarito.cs
+++ Prova de POO 2º bimestre/Gabarito.cs
@@ -24,81 +24,88 @@ namespace Prova_de_POO_2º_bimestre
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Novo()
         {
+           
+            
             int acertos = 0;
-            int sm = 0;
+            
             try
             {
-                string[] gab = { "A", "B", "D", "E", "C", "A", "A", "D", "E","B" };
+                //Atualização para enviar para o gitHub
+                string[] gab = { "A", "B", "D", "E", "C", "A", "A", "D", "E", "B" };
                 for (int i = 0; i < gab.Length; i++)
                 {
                     acertos = 0;
                     if (txt1.Text.ToUpper() == gab[0])
                     {
                         acertos++;
-                        
+
                     }
                     if (txt2.Text.ToUpper() == gab[1])
                     {
                         acertos++;
-                       
+
                     }
-                    
+
                     if (txt3.Text.ToUpper() == gab[2])
                     {
-                        acertos++;                        
+                        acertos++;
                     }
-                    
+
                     if (txt4.Text.ToUpper() == gab[3])
                     {
                         acertos++;
-                        
+
                     }
                     if (txt5.Text.ToUpper() == gab[4])
                     {
                         acertos++;
-                       
+
                     }
                     if (txt6.Text.ToUpper() == gab[5])
                     {
-                        acertos ++;
-                       
+                        acertos++;
+
                     }
                     if (txt7.Text.ToUpper() == gab[6])
                     {
                         acertos++;
-                        
+
                     }
                     if (txt8.Text.ToUpper() == gab[7])
                     {
                         acertos++;
-                       
+
                     }
                     if (txt9.Text.ToUpper() == gab[8])
                     {
                         acertos++;
-                        
+
                     }
                     if (txt10.Text.ToUpper() == gab[9])
                     {
                         acertos++;
-                        
+
                         break;
                     }
 
-                   
+
                 }
 
                 rt.Text = Convert.ToString(acertos);
-                
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Tem que ser letra maiscula");
-            }
-            
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro!!");
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Novo();
+           
         }
     }
 }
